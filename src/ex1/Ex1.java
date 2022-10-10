3fe5159a06fe08a451135f3db0e8c9a85e00b2f3
luizfelipetorres <fellipe116@gmail.com>
2022-10-10 12:31:46 -0300

    Iniciando

--- src/ex1/Ex1.java
+++ src/ex1/Ex1.java
@@ -2,37 +2,58 @@ package ex1;
 
 import java.util.Scanner;
 
+/**
+ * A partir de dois números decimais e efetuar as quatros operações matemáticas básicas,
+ * apresentando ao final os resultados obtidos
+ */
 public class Ex1 {
-    protected int n;
+    private double n1;
+    private double n2;
 
-    public Ex1(Scanner s) {
-        System.out.printf("Digite um numero inteiro: ");
-        this.n = s.nextInt();
+    public Ex1(double n1, double n2) {
+        super();
+        this.n1 = n1;
+        this.n2 = n2;
     }
 
-    /**
-     * Compara dois valores e diz se o primeiro é menor, maior ou igual ao segundo
-     * @param comparator o numero que será comparado
-     * @return 1 se n > comparator, -1 se n < comparator e 0 se n == comparator
-     */
-    protected int comparator(int comparator){
-        if (n > comparator)
-            return 1;
-        else if (n < comparator)
-            return -1;
-        return 0;
+    public Ex1() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        this.n1 = s.nextInt();
+        System.out.println("Digite o segundo número: ");
+        this.n2 = s.nextInt();
     }
 
-    protected String result (){
-        if (n > 0)
-            return "positivo";
-        else if (n == 0)
-            return Integer.toString(0);
-        return "negativo";
+    public double sum() {
+        return this.getN1() + this.getN2();
+    }
+
+    public double subtract() {
+        return this.getN1() - this.getN2();
+    }
+
+    public double divide() {
+        return this.getN1() / this.getN2();
+    }
+
+    public double multiply() {
+        return this.getN1() * this.getN2();
     }
 
     @Override
     public String toString() {
-        return "O numero digitado é " + result();
+        return "Soma: " + this.sum() +
+                "\nSubtração: " + this.subtract() +
+                "\nDivisão: " + this.divide() +
+                "\nMultiplicação: " + this.multiply();
+
+    }
+
+    public double getN1() {
+        return n1;
+    }
+
+    public double getN2() {
+        return n2;
     }
 }
